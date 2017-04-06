165da4270d4be8cf31e11c84550feb9b82c3f4f7
polkov <dadkop@gmail.com>
2017-04-06 20:20:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
 
     $db = new PDO("mysql:host=localhost;dbname=windcup", "******", "******");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $dotazText = "INSERT INTO teami(jmenoteamu, jmenokapitana, emailkapitana, steamidkapitana, clen1, clen2, clen3, clen4, nahradnici) VALUES ('$jmenoteamu, $meno, $mail, $predmed, $clen1, $clen2, $clen3, $clen4, $sprava')";
+  $dotazText = "INSERT INTO teami(jmenoteamu, jmenokapitana, emailkapitana, steamidkapitana, clen1, clen2, clen3, clen4, nahradnici) VALUES ('$jmenoteamu', '$meno', '$mail', '$predmet', '$clen1', '$clen2', '$clen3', '$clen4', '$sprava')";
 
     try {
         $db->query($dotazText);
