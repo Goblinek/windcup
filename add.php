d4cf7b63643781e9f155d9aa483bfd749063989f
Goblinek <xkytx1@gmail.com>
2017-04-14 22:13:03 +0200

    Úprava a optimalizace kódu

--- add.php
+++ add.php
@@ -2,17 +2,16 @@
  
 <html>
 <head>
-	<title>WindCup | Registrácia</title>
+	<title>WindCup | Přidávání novinek</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
 </head>
 <body> 
     <div id="bg">
+    <div class="main">
     <form method="POST" action="add.php">
         <h4>Nadpis:</h4>
         <input type="text" name="Nadpis" class="Nadpis" required>
-        <h4>Datum:</h4>
-        <input type="text" name="datum" class="datum" required>
         <h4>Admin:</h4>
         <input type="text" name="napsal" class="napsal" required>
         
@@ -22,20 +21,23 @@
         <input type="submit" name="odoslat" value="Odoslať" class="odoslat">
     </form>
     </div>
+    </div>
 
     <?php
     $Nadpis = $_POST["Nadpis"];
-    $datum = $_POST["datum"];
-    $admin = $_POST["napsal"];
+    $datum = date("d.m.y", strtotime($_POST['datum']));
+    $napsal = $_POST["napsal"];
     $novinka = $_POST["novinka"];
  
-    $db = new PDO("mysql:host=localhost;dbname=windcup", "root", "killmcserv24865");
+    $db = new PDO("mysql:host=localhost;dbname=windcup", "************", "**************");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (isset($_POST['odoslat'])) {
     $dotazText = "INSERT INTO news(Nadpis, datum, napsal, novinka) VALUES ('$Nadpis', '$datum', '$napsal', '$novinka')";
    try {
         $db->query($dotazText);
     } catch (PDOException $e) {
       echo $e->getMessage();
+    }
     }
     ?>
 	
