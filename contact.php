d997637d0f275a145f4f05449faf0be1abfffd22
Goblinek <xkytx1@gmail.com>
2017-04-14 18:12:54 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+   <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+	<title>WindCup | Kontakt</title>
+</head>
+<body>
+    <div id="nav_wrap">
+        <div class="nav">
+        <a href="index.php"><img src="images/logo.png"></a>
+    	    <ul>
+    	    	<li><a href="index.php">Domov</a></li>
+    	    	<li><a href="teams.php">Teamy</a></li>
+    	    	<li><a href="register.php">Registrácia</a></li>
+    	    	<li><a href="rules.php">Pravidlá</a>
+    	    	<li><a href="contact.php" class="active">Kontakt</a></li>
+    	    </ul>
+    	</div>
+    </div>
+
+  </body>
+</html>
