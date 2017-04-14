31f7e9a15ac62c961de1eb706c36d94438f7ebe6
Goblinek <xkytx1@gmail.com>
2017-04-14 21:57:07 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+l <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -8,13 +8,13 @@
 <body>
     <div id="nav_wrap">
         <div class="nav">
-        <a href="index.html"><img src="images/logo.png"></a>
+        <a href="index.php"><img src="images/logo.png"></a>
     	    <ul>
-    	    	<li><a href="index.html">Domov</a></li>
-    	    	<li><a href="teams.html">Teamy</a></li>
-    	    	<li><a href="register.html" class="active">Registrácia</a></li>
-    	    	<li><a href="rules.html">Pravidlá</a>
-    	    	<li><a href="contact.html">Kontakt</a></li>
+    	    	<li><a href="index.php">Domov</a></li>
+    	    	<li><a href="teams.php">Teamy</a></li>
+    	    	<li><a href="register.php" class="active">Registrácia</a></li>
+    	    	<li><a href="rules.php">Pravidlá</a>
+    	    	<li><a href="contact.php">Kontakt</a></li>
     	    </ul>
     	</div>
     </div>
@@ -24,7 +24,7 @@
     		    <div style="float: left; margin-left: 150px;">
                     <h4>Meno Teamu</h4>
                     <input type="text" name="jmenoteamu" class="jmenoteamu" placeholder="Meno Teamu" required>
-                    <h4>Meno a Priezvisko kapitána tímu</h4>
+                    <h4>Meno a Priezvisko kapitána teamu</h4>
                     <input type="text" name="meno" class="meno" placeholder="Meno Priezvisko" required>
                     <h4>Email kapitána teamu</h4>
                     <input type="text" name="mail" class="mail" placeholder="email@domena.sk" required>
@@ -42,7 +42,7 @@
                     <input type="text" name="clen4" class="clen4" placeholder='Meno "nick" Priezvisko' required>
                 </div>
                 <div style="clear: both; padding-top: 20px;">
-                    <h4>Logo tímu (png obrázok)</h4>
+                    <h4>Logo teamu (png obrázok)</h4>
                     <input type="file" name="picUpload" id="picUpload" class="pic" required>
                     <h4>Mená náhradníkov v odrážkách</h4>
                     <textarea name="sprava" class="sprava" placeholder='Meno "nick" Priezvisko' required></textarea>
@@ -63,17 +63,20 @@
                 $sprava = $_POST["sprava"];
                 $jmenoteamu = $_POST["jmenoteamu"];
  
-                $db = new PDO("mysql:host=localhost;dbname=windcup", "******", "******");
+                $db = new PDO("mysql:host=localhost;dbname=windcup", "*******", "**********");
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if (isset($_POST['odoslat'])) {
                 $dotazText = "INSERT INTO teami(jmenoteamu, jmenokapitana, emailkapitana, steamidkapitana, clen1, clen2, clen3, clen4, nahradnici) VALUES ('$jmenoteamu', '$meno', '$mail', '$predmet', '$clen1', '$clen2', '$clen3', '$clen4', '$sprava')";
-                try {
+                
+                try {   
                     $db->query($dotazText);
                 } catch (PDOException $e) {
                   echo $e->getMessage();
                 }
+                 }
                 
                 // Odoslanie obrázka
-                $odoslat_do = "images/teams/";
+                $odoslat_do = "image/teams/";
                 $odoslat_subor = $odoslat_do . basename($_FILES["picUpload"]["name"]);
                 $uploadOk = 1;
                 $typObrazka = pathinfo($odoslat_subor, PATHINFO_EXTENSION);
